bc1a0e612a384c0f491676e58892dd595c359dec
Yahia Elsayed <yahiaelsayed19@gmail.com>
2023-09-21 19:36:21 +0300

    hide header on scroll down show it again on scroll up

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,10 +5,31 @@ import Navigation from "./Navigation";
 import Searchbar from "./Searchbar";
 import AuthButton from "./AuthButton";
 import MobileNav from "./MobileNav";
+import { useEffect } from "react";
 
 const Header = () => {
+    useEffect(() => {
+        const header = document.getElementById('header');
+        let prevScrollY = 0;
+
+        function handleScroll() {
+            const currentScrollY = window.scrollY;
+            if (header) {
+                if (currentScrollY > prevScrollY) {
+                    header.style.transform = 'translateY(-100%)';
+                } else {
+                    header.style.transform = 'translateY(0)';
+                }
+
+                prevScrollY = currentScrollY;
+            }
+
+        }
+
+        window.addEventListener('scroll', handleScroll);
+    }, [])
     return (
-        <header className="bg-white dark:bg-black shadow flex h-[64px] sm:h-auto z-50">
+        <header id="header" className="sticky top-0 left-0 bg-white dark:bg-black shadow flex h-[64px] sm:h-auto z-50 scroll-smooth">
             <div className="container mx-auto max-w-5xl flex flex-1 flex-col sm:gap-2 px-[15px] py-4">
                 <div className="flex flex-1 items-center justify-between gap-2 flex-row">
                     <MobileNav />
