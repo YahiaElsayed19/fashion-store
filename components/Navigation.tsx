7e8055efd832e138717e9ef7fda9292adef175c7
Yahia Elsayed <yahiaelsayed19@gmail.com>
2023-09-21 13:24:57 +0300

    change code to render list of links

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -1,19 +1,11 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Link from 'next/link'
-
+import { links } from '@util/links'
 const Navigation = () => {
-    //desktop-navigation
+
     return (
         <nav className='flex gap-3 items-center max-sm:hidden'>
-            <Link href="/favorite" className='nav-link'>
-                Favorites
-            </Link>
-            <Link href="/cart" className='nav-link '>
-                Cart
-            </Link>
-            <Link href="/profile" className='nav-link '>
-                Profile
-            </Link>
+            {links.map((link) => <Link href={link.url} className='nav-link'>{link.title}</Link>)}
         </nav>)
 }
 
---
+++ util/links.ts
@@ -0,0 +1,14 @@
+export const links: { title: string, url: string }[] = [
+    {
+        title: "Wishlist",
+        url: "/wishlist",
+    },
+    {
+        title: "Cart",
+        url: "/cart",
+    },
+    {
+        title: "Profile",
+        url: "/profile",
+    },
+]
