7f6d655d89d8f80883dd9898482110147b44ce91
Yahia Elsayed <yahiaelsayed19@gmail.com>
2023-09-21 11:00:06 +0300

    move authbutton logic

---
+++ components/AuthButton.tsx
@@ -0,0 +1,44 @@
+import React from 'react'
+import { useEffect, useState } from "react";
+import { signIn, signOut, getProviders, useSession } from "next-auth/react";
+
+const AuthButton = () => {
+    const { data: session } = useSession();
+    const [providers, setProviders] = useState<any>(null);
+    useEffect(() => {
+        const getProvidersFn = async () => {
+            const response = await getProviders();
+            setProviders(response);
+            console.log(response);
+        };
+        getProvidersFn();
+    }, []);
+
+    return (
+        <div>
+            {session?.user ? (
+                <button
+                    className="auth-button"
+                    onClick={() => signOut()}
+                >
+                    Sign out
+                </button>
+            ) : (
+                <>
+                    {providers &&
+                        Object.values(providers).map((provider: any) => (
+                            <button
+                                type="button"
+                                key={provider.name}
+                                onClick={() => signIn(provider.id)}
+                                className="auth-button"
+                            >
+                                Sign in
+                            </button>
+                        ))}
+                </>
+            )}
+        </div>)
+}
+
+export default AuthButton
