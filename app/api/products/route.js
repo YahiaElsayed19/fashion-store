0bc48a23bd4c7178af757d055c870bfef66f9c42
Yahia Elsayed <yahiaelsayed19@gmail.com>
2023-09-22 16:08:30 +0300

    edit products req to get page by page

--- app/api/products/route.js
+++ app/api/products/route.js
@@ -1,12 +1,14 @@
 import Product from "@models/product"
 import { connectToDB } from "@util/database"
+const itemsPerPage = 10;
 
 export const GET = async (req) => {
+    const url = new URL(req.url)
+    let page = url.searchParams.get('page') || 1
+    let startIndex = (page - 1) * itemsPerPage
     try {
         await connectToDB()
-        const products = await Product.find({})
-        console.log(products);
-        console.log(products.length);
+        const products = await Product.find({}).skip(startIndex).limit(itemsPerPage)
         return new Response(JSON.stringify(products), { status: 200 })
     } catch (error) {
         return new Response(JSON.stringify(error), { status: 500 })
