932af25bdd73f893e00da514455ee7a65a1d9f94
Yahia Elsayed <yahiaelsayed19@gmail.com>
2023-09-22 12:20:33 +0300

    finish exit animation

--- components/MobileNav.tsx
+++ components/MobileNav.tsx
@@ -5,7 +5,7 @@ import { CgMenu } from "react-icons/cg";
 import { usePathname } from "next/navigation";
 import { links } from "@util/links";
 import Searchbar from "./Searchbar";
-import { motion } from "framer-motion";
+import { AnimatePresence, motion } from "framer-motion";
 
 const MobileNav = () => {
     const [showMenu, setShowMenu] = useState(false);
@@ -45,30 +45,33 @@ const MobileNav = () => {
                 onClick={toggleMenuHandler}
                 id="icon"
             />
-            {showMenu && (
-                <motion.div
-                    initial={{ y: "-100%" }}
-                    animate={{ y: 0 }}
-                    transition={{ duration: 5 }}
-                    className="flex flex-col w-full h-auto p-4 gap-2 absolute top-[64px] left-0  bg-white dark:bg-black"
-                >
-                    <nav className="flex flex-col gap-2 text-center">
-                        {links.map((link) => (
-                            <Link
-                                key={link.title}
-                                href={link.url}
-                                className={pathname == link.url ? "active-link" : "nav-link"}
-                                onClick={closeMenuHandler}
-                            >
-                                {link.title}
-                            </Link>
-                        ))}
-                    </nav>
-                    <div id="Searchbar">
-                        <Searchbar />
-                    </div>
-                </motion.div>
-            )}
+            <AnimatePresence mode="wait">
+                {showMenu && (
+                    <motion.div
+                        initial={{ x: "-100%" }}
+                        animate={{ x: 0 }}
+                        exit={{ x: "100%" }}
+                        transition={{ duration: 0.2 }}
+                        className="flex flex-col w-full h-auto p-4 gap-2 absolute top-[64px] left-0  bg-white dark:bg-black"
+                    >
+                        <nav className="flex flex-col gap-2 text-center">
+                            {links.map((link) => (
+                                <Link
+                                    key={link.title}
+                                    href={link.url}
+                                    className={pathname == link.url ? "active-link" : "nav-link"}
+                                    onClick={closeMenuHandler}
+                                >
+                                    {link.title}
+                                </Link>
+                            ))}
+                        </nav>
+                        <div id="Searchbar">
+                            <Searchbar />
+                        </div>
+                    </motion.div>
+                )}
+            </AnimatePresence>
         </div>
     );
 };
