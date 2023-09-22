4ba48b31ce8d117072591c3abdf703c0b905ad9e
Yahia Elsayed <yahiaelsayed19@gmail.com>
2023-09-22 19:18:32 +0300

    add new end points

---
+++ app/api/categories/[category]/route.js
@@ -0,0 +1,16 @@
+import Product from "@models/product"
+import { connectToDB } from "@util/database"
+const itemsPerPage = 10;
+
+export const GET = async (req, { params }) => {
+    const url = new URL(req.url)
+    let page = url.searchParams.get('page') || 1
+    let startIndex = (page - 1) * itemsPerPage
+    try {
+        await connectToDB()
+        const products = await Product.find({ category: params.category }).skip(startIndex).limit(itemsPerPage)
+        return new Response(JSON.stringify(products), { status: 200 })
+    } catch (error) {
+        return new Response(JSON.stringify(error), { status: 500 })
+    }
+}
