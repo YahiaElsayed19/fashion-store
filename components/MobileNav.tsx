b39609f8de3af0ea3798e2172c5f7de3d3ac7f90
Yahia Elsayed <yahiaelsayed19@gmail.com>
2023-09-22 11:57:21 +0300

    add scroll listtner to close the menu

--- components/MobileNav.tsx
+++ components/MobileNav.tsx
@@ -19,30 +19,37 @@ const MobileNav = () => {
 
     useEffect(() => {
         function clickOutside(event: Event) {
-            let menu = document.getElementById('menu');
+            let icon = document.getElementById('icon');
             let searchbar = document.getElementById('Searchbar');
             //@ts-ignore
-            if (menu && !menu.contains(event.target) && searchbar && !searchbar.contains(event.target)) {
+            if (icon && !icon.contains(event.target) && searchbar && !searchbar.contains(event.target)) {
                 closeMenuHandler()
             }
         }
+        function scrollHandler(event: Event) {
+            closeMenuHandler()
+        }
         document.addEventListener('click', clickOutside)
+        window.addEventListener('scroll', scrollHandler)
 
-        return () => { document.removeEventListener('click', clickOutside) }
+        return () => {
+            document.removeEventListener('click', clickOutside)
+            window.removeEventListener('scroll', scrollHandler)
+        }
     }, [])
 
     return (
-        <div className="sm:hidden z-40">
+        <div className="sm:hidden ">
             <CgMenu
                 className="w-8 h-8 text-black dark:text-white sm:hidden cursor-pointer"
                 onClick={toggleMenuHandler}
-                id="menu"
+                id="icon"
             />
             {showMenu && (
                 <motion.div
-                    initial={{ x: "-100%" }}
-                    animate={{ x: 0 }}
-                    transition={{ duration: 0.2 }}
+                    initial={{ y: "-100%" }}
+                    animate={{ y: 0 }}
+                    transition={{ duration: 5 }}
                     className="flex flex-col w-full h-auto p-4 gap-2 absolute top-[64px] left-0  bg-white dark:bg-black"
                 >
                     <nav className="flex flex-col gap-2 text-center">
