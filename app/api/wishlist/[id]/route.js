c1b8b694923381a3c6404b15bdffb92838d55b25
Yahia Elsayed <yahiaelsayed19@gmail.com>
2023-09-23 11:12:13 +0300

    update wishlist request to use product id

--- app/api/wishlist/[id]/route.js
+++ app/api/wishlist/[id]/route.js
@@ -1,10 +1,14 @@
 import Wishlist from "@models/wishlist"
+import Product from "@models/product"
 import { connectToDB } from "@util/database"
 
 export const POST = async (req, { params }) => {
-    const addedProduct = await req.json();
+    const url = new URL(req.url)
+    let id = url.searchParams.get('id')
+
     try {
         await connectToDB()
+        let addedProduct = await Product.findOne({ _id: id })
         let wishlist = await Wishlist.findOne({ owner: params.id })
         if (!wishlist) {
             await Wishlist.create({
