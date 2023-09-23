1e9ce0797b38700f24ffc929ba98e7ab1f18527d
Yahia Elsayed <yahiaelsayed19@gmail.com>
2023-09-23 11:09:57 +0300

    update cart api to use product id

--- app/api/cart/[id]/route.js
+++ app/api/cart/[id]/route.js
@@ -1,10 +1,14 @@
 import Cart from "@models/cart"
+import Product from "@models/product"
 import { connectToDB } from "@util/database"
 
 export const POST = async (req, { params }) => {
-    const addedProduct = await req.json();
+    const url = new URL(req.url)
+    let id = url.searchParams.get('id')
+
     try {
         await connectToDB()
+        let addedProduct = await Product.findOne({ _id: id })
         let cart = await Cart.findOne({ owner: params.id })
         if (!cart) {
             await Cart.create({
