b3d34519ff2f5429b9f7710898d20d8cd86f73f0
Yahia Elsayed <yahiaelsayed19@gmail.com>
2023-09-23 12:36:31 +0300

    add animation to header

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,28 +5,30 @@ import Navigation from "./Navigation";
 import Searchbar from "./Searchbar";
 import AuthButton from "./AuthButton";
 import MobileNav from "./MobileNav";
-import { useEffect } from "react";
-
+import { useEffect, useState } from "react";
+import { motion } from "framer-motion";
 const Header = () => {
-    
+    let [hideHeader, setHideHeader] = useState(false);
     useEffect(() => {
-        const header = document.getElementById('header');
+        const header = document.getElementById("header");
         let prevScrollY = 0;
         function handleScroll() {
-            const currentScrollY = window.scrollY;
+            const currentScrollY = window.scrollY || document.documentElement.scrollTop;
             if (header) {
-                if (currentScrollY > prevScrollY) {
-                    header.style.transform = 'translateY(-1000%)';
-                } else {
-                    header.style.transform = 'translateY(0)';
-                }
-                prevScrollY = currentScrollY;
+                setHideHeader(currentScrollY > prevScrollY)
+                prevScrollY = currentScrollY <= 150 ? 150 : currentScrollY;
             }
         }
-        window.addEventListener('scroll', handleScroll);
-    }, [])
+        window.addEventListener("scroll", handleScroll);
+    }, []);
     return (
-        <header id="header" className="sticky top-0 left-0 bg-white dark:bg-black shadow flex h-[64px] sm:h-auto z-50 scroll-smooth">
+        <motion.header
+            initial={{ y: 0 }}
+            animate={{ y: hideHeader ? -150 : 0 }}
+            transition={{ duration: 0.3 }}
+            id="header"
+            className="sticky top-0 left-0 bg-white dark:bg-black shadow flex h-[64px] sm:h-auto z-50 scroll-smooth"
+        >
             <div className="container mx-auto max-w-5xl flex flex-1 flex-col sm:gap-2 px-[15px] py-4">
                 <div className="flex flex-1 items-center justify-between gap-2 flex-row">
                     <MobileNav />
@@ -55,7 +57,7 @@ const Header = () => {
                     <Searchbar />
                 </div>
             </div>
-        </header>
+        </motion.header>
     );
 };
 
