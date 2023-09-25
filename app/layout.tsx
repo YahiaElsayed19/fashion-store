26bbd5928128422b3fb3a74f8a78726c12d575ec
Yahia Elsayed <yahiaelsayed19@gmail.com>
2023-09-25 18:28:15 +0300

    revert remove alias

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 "use client"
-import Header from '../components/Header'
-import Provider from '../components/Provider'
+import Header from '@components/Header'
+import Provider from '@components/Provider'
 import '@styles/globals.css'
 import { Inter } from 'next/font/google'
 
