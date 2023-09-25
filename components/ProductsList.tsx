21365b7555f41c17d2159f14684401676c32118c
Yahia Elsayed <yahiaelsayed19@gmail.com>
2023-09-26 00:21:52 +0300

    reusable dynamic carousel

--- components/ProductsList.tsx
+++ components/ProductsList.tsx
@@ -2,21 +2,21 @@
 import React from 'react'
 import '@splidejs/react-splide/css';
 import { useQuery } from 'react-query'
-import { getProductsByType } from '@util/api';
 import ProductCard from './ProductCard';
 import Link from 'next/link';
 //@ts-ignore
 import { Splide } from '@splidejs/react-splide'
 import { productType } from '@types';
 
-const ProductsList = () => {
-    const { data } = useQuery("trending", () => getProductsByType("trending"))
+const ProductsList = ({ queryFunction, title }: { queryFunction: any, title: string }) => {
+    const basePath = typeof window !== undefined && window.location.origin ? window.location.origin : ""
+    const { data } = useQuery(title, () => queryFunction(title))
     return (
-        <section className='py-[100px] bg-white dark:bg-black'>
+        <section className='py-[50px] bg-white dark:bg-black'>
             <div className='container w-full max-w-6xl px-5 mx-auto'>
                 <div className="flex justify-between px-5 mb-2">
-                    <h2 className='text-black dark:text-white font-bold text-lg'>Trending</h2>
-                    <Link href="http://localhost:3000/trending" className='text-primary-color hover'>See all</Link>
+                    <h2 className='text-black dark:text-white font-bold text-lg capitalize'>{title}</h2>
+                    <Link href={`${basePath}/${title}`} className='text-primary-color hover'>See all</Link>
                 </div>
                 <div className="p-5 bg-gray-200 dark:bg-dark-container rounded-2xl" >
                     <Splide options={{
