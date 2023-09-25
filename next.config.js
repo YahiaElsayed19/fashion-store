4b64745ae25cb7c14451263b78db9823f2223d54
Yahia Elsayed <yahiaelsayed19@gmail.com>
2023-09-25 22:55:10 +0300

    allow images from hm

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const nextConfig = {
         serverComponentsExternalPackages: ["mongoose"],
     },
     images: {
-        domains: ['lh3.googleusercontent.com'],
+        domains: ['lh3.googleusercontent.com','eg.hm.com'],
     },
     webpack(config) {
         config.experiments = {
