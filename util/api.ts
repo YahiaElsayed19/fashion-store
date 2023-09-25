e3a8ad6fb11493f2cdc11b199d2bc33bf760bd7c
Yahia Elsayed <yahiaelsayed19@gmail.com>
2023-09-25 23:40:50 +0300

    edit api host to make it works on host

--- util/api.ts
+++ util/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 const API = axios.create({
-    baseURL: `http://localhost:3000/api/`,
+    baseURL: `api/`,
 });
 
 export const getProductsByType = (type: string, page?: number) => {
