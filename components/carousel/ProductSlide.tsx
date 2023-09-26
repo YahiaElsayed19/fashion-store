c1a6939f75d3dc49d2139f799ea41735dfd655a0
Yahia Elsayed <yahiaelsayed19@gmail.com>
2023-09-26 21:18:52 +0300

    handle request failure

--- components/carousel/ProductSlide.tsx
+++ components/carousel/ProductSlide.tsx
@@ -9,10 +9,19 @@ import { useSession } from 'next-auth/react'
 const ProductSlide: React.FC<productSlideType> = ({ id, title, imageSrc, price }) => {
     const { data: session } = useSession()
     const [submitting, setSubmitting] = useState(false)
+    const [success, setSuccess] = useState<boolean | null>()
     const addToCartHandler = async () => {
         setSubmitting(true)
-        //@ts-ignore
-        await addToCart(session?.user.id, id)
+        try {
+            //@ts-ignore
+            await addToCart(session?.user.id, id)
+            setSuccess(true)
+        } catch (error) {
+            setSuccess(false)
+        }
+        setTimeout(() => {
+            setSuccess(null)
+        }, 2000)
         setSubmitting(false)
     }
     return (
@@ -21,6 +30,8 @@ const ProductSlide: React.FC<productSlideType> = ({ id, title, imageSrc, price }
             <p className='text-black dark:text-white text-md font-bold mt-2 w-[150px] truncate'>{title}</p>
             <p className='text-primary text-sm'>{price} EGP</p>
             <button disabled={submitting} className='bg-white text-black dark:bg-black dark:text-white font-medium mt-5 py-2 px-3 rounded-2xl hover:bg-primary hover:text-white duration-300 disabled:bg-gray-400 disabled:cursor-not-allowed' onClick={addToCartHandler}>+ add to cart</button>
+            {success === true && <p className='msg-toast bg-green-400'>Successed</p>}
+            {success === false && <p className='msg-toast bg-red-600 '>Failed</p>}
         </SplideSlide>
     )
 }
