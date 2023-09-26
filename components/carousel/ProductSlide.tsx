c36e0c324ca43afb70bdf05876c676f012ff4c7e
Yahia Elsayed <yahiaelsayed19@gmail.com>
2023-09-26 20:28:29 +0300

    edit button styles

--- components/carousel/ProductSlide.tsx
+++ components/carousel/ProductSlide.tsx
@@ -20,7 +20,7 @@ const ProductSlide: React.FC<productSlideType> = ({ id, title, imageSrc, price }
             <Image src={imageSrc} width={150} height={225} alt={title} className='rounded-2xl' />
             <p className='text-black dark:text-white text-md font-bold mt-2 w-[150px] truncate'>{title}</p>
             <p className='text-primary-color text-sm'>{price} EGP</p>
-            <button disabled={submitting} className='bg-primary-color text-black dark:text-white mt-5 py-2 px-3 rounded-2xl disabled:bg-gray-400 disabled:cursor-not-allowed' onClick={addToCartHandler}>+ add to cart</button>
+            <button disabled={submitting} className='bg-white text-black dark:bg-black dark:text-white font-medium mt-5 py-2 px-3 rounded-2xl hover:bg-primary-color hover:text-white duration-300 disabled:bg-gray-400 disabled:cursor-not-allowed' onClick={addToCartHandler}>+ add to cart</button>
         </SplideSlide>
     )
 }
