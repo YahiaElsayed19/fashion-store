b45a56671117cab03115ee7ada1e013d0a7f6b33
Yahia Elsayed <yahiaelsayed19@gmail.com>
2023-09-27 19:56:37 +0300

    finish infinte loading in home page

--- components/product/AllProducts.tsx
+++ components/product/AllProducts.tsx
@@ -1,13 +1,32 @@
 "use client"
-import React from 'react'
-import { useQuery } from 'react-query'
+import React, { useEffect, useState } from 'react'
 import { getProducts } from '@util/api'
 import ProductsList from './ProductsList'
+import { productType } from '@types'
 
 const AllProducts = () => {
-    const { data } = useQuery("allProducts", () => getProducts())
+    const [page, setPage] = useState(1)
+    const [products, setProducts] = useState<productType[]>([])
+    const loadMore = () => {
+        setPage((prev) => prev + 1)
+    }
+    useEffect(() => {
+        (async () => {
+            const { data } = await getProducts(page)
+            if (page === 1) {
+                setProducts(data)
+            }
+            if (page > 1 && page < 23) {
+                setProducts((prev) => [...prev, ...data])
+            }
+        })()
+    }, [page])
     return (
-        <ProductsList products={data?.data} />
+        <section className='bg-white dark:bg-black flex flex-col items-center py-[50px]'>
+            <h1 className='text-2xl font-bold uppercase text-center text-black dark:text-white'>All products</h1>
+            <ProductsList products={products} />
+            <button className='button' onClick={loadMore}>Show more</button>
+        </section>
     )
 }
 
--- components/product/ProductsList.tsx
+++ components/product/ProductsList.tsx
@@ -5,13 +5,13 @@ import { productType } from '@types'
 import ProductCard from './ProductCard'
 const ProductsList = ({ products }: { products: productType[] }) => {
     return (
-        <section className='py-[50px] flex flex-col items-center bg-white dark:bg-black'>
+        <div className='flex flex-col items-center bg-white dark:bg-black'>
             <div className='container w-full max-w-6xl p-5 mx-auto '>
                 <div className='flex justify-center flex-wrap gap-10 p-5 rounded-2xl bg-gray-200 dark:bg-dark-container'>
                     {products?.map((product: productType) => <ProductCard key={product._id} id={product._id} title={product.title} imageSrc={product.images[0]} price={product.price} />)}
                 </div>
             </div>
-        </section>
+        </div>
     )
 }
 
