2107c28a8ceccb70d68b27312cc7f428f544cd53
Yahia Elsayed <yahiaelsayed19@gmail.com>
2023-09-27 17:20:07 +0300

    add key to map

--- components/product/ProductsList.tsx
+++ components/product/ProductsList.tsx
@@ -8,7 +8,7 @@ const ProductsList = ({ products }: { products: productType[] }) => {
         <section className='py-[50px] flex flex-col items-center bg-white dark:bg-black'>
             <div className='container w-full max-w-6xl p-5 mx-auto '>
                 <div className='flex justify-center flex-wrap gap-10 p-5 rounded-2xl bg-gray-200 dark:bg-dark-container'>
-                    {products?.map((product: productType) => <ProductCard id={product._id} title={product.title} imageSrc={product.images[0]} price={product.price} />)}
+                    {products?.map((product: productType) => <ProductCard key={product._id} id={product._id} title={product.title} imageSrc={product.images[0]} price={product.price} />)}
                 </div>
             </div>
         </section>
