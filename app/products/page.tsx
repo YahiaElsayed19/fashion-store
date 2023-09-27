3982111ca396d73d3d00162fd38d73daf2e8d36d
Yahia Elsayed <yahiaelsayed19@gmail.com>
2023-09-27 21:54:44 +0300

    add all products route

---
+++ app/products/page.tsx
@@ -0,0 +1,12 @@
+import AllProducts from '@components/product/AllProducts'
+import React from 'react'
+
+const page = () => {
+    return (
+        <section className=' min-h-screen bg-white dark:bg-black'>
+            <AllProducts />
+        </section>
+    )
+}
+
+export default page
