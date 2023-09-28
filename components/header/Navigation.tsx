5d8a1c14f9ebc05be0ce53995bbff256e8034264
Yahia Elsayed <yahiaelsayed19@gmail.com>
2023-09-28 17:32:51 +0300

    clean up navigation

--- components/header/Navigation.tsx
+++ components/header/Navigation.tsx
@@ -1,13 +1,22 @@
-import React from 'react'
-import Link from 'next/link'
-import { usePathname } from 'next/navigation'
-import { navLinks } from '@util/links'
+import React from "react";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { navLinks } from "@util/links";
 const Navigation = () => {
-    const pathname = usePathname()
+    const pathname = usePathname();
     return (
-        <nav className='flex gap-3 items-center max-sm:hidden'>
-            {navLinks.map((link) => <Link key={link.title} href={link.url} className={pathname == link.url ? "active-link" : "nav-link"}>{link.title}</Link>)}
-        </nav>)
-}
+        <nav className="flex gap-3 items-center max-sm:hidden">
+            {navLinks.map((link) => (
+                <Link
+                    key={link.title}
+                    href={link.url}
+                    className={pathname == link.url ? "active-link" : "nav-link"}
+                >
+                    {link.title}
+                </Link>
+            ))}
+        </nav>
+    );
+};
 
-export default Navigation
+export default Navigation;
