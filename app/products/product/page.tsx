8d75a4606c7aac6d8942c412774ff7eb534ecb12
Yahia Elsayed <yahiaelsayed19@gmail.com>
2023-09-30 17:41:39 +0300

    remove radius

--- app/products/product/page.tsx
+++ app/products/product/page.tsx
@@ -43,7 +43,7 @@ const page = () => {
     return (
         <section className="min-h-screen bg-gray-200 dark:bg-dark-container">
             <div className="container p-4 mx-auto flex flex-col lg:flex-row lg:justify-center gap-4">
-                <div className="px-4 mx-auto w-full lg:w-[400px] bg-white dark:bg-black lg:sticky lg:top-[144px] lg:left-0 rounded-xl h-fit">
+                <div className="px-4 mx-auto w-full lg:w-[400px] bg-white dark:bg-black lg:sticky lg:top-[144px] lg:left-0 h-fit">
                     <div className="text-right text-black dark:text-white py-3 pb-5 border-b border-gray-200 dark:border-dark-container ">
                         <p className="text-primary font-bold text-lg mb-4">
                             {product?.title}
