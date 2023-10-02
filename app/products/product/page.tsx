1be16baa5c511fe9cc2dc4e61364575636a80b96
Yahia Elsayed <yahiaelsayed19@gmail.com>
2023-10-02 23:18:05 +0300

    add relative to fix icon position

--- app/products/product/page.tsx
+++ app/products/product/page.tsx
@@ -48,66 +48,66 @@ const page = () => {
                     <SingleProductPlaceholder />
                 ) : (
                     <>
-                    <div className="px-4 mx-auto w-full lg:w-[400px] bg-white dark:bg-black lg:sticky lg:top-[144px] lg:left-0 h-fit">
-                        <div className="text-right text-black dark:text-white py-3 pb-5 border-b border-gray-200 dark:border-dark-container ">
-                            <p className="text-primary font-bold text-lg mb-4">
-                                {product?.title}
-                            </p>
-                            <p className="font-medium leading-7">{product?.desc}</p>
+                        <div className="px-4 mx-auto w-full lg:w-[400px] bg-white dark:bg-black lg:sticky lg:top-[144px] lg:left-0 h-fit relative">
+                            <div className="text-right text-black dark:text-white py-3 pb-5 border-b border-gray-200 dark:border-dark-container ">
+                                <p className="text-primary font-bold text-lg mb-4">
+                                    {product?.title}
+                                </p>
+                                <p className="font-medium leading-7">{product?.desc}</p>
+                            </div>
+                            <div className="text-right text-black dark:text-white pt-4 pb-5 border-b border-gray-200 dark:border-dark-container">
+                                <p className="text-sm">price cash</p>
+                                <p className="text-right text-primary mt-5">
+                                    {product?.price} EGP
+                                </p>
+                            </div>
+                            <div className="flex justify-end gap-2 py-5 border-b border-gray-200 dark:border-dark-container">
+                                <Link
+                                    href={`/products/categories/${product?.category}`}
+                                    className="tag"
+                                >
+                                    {product?.category}
+                                </Link>
+                                <Link href={`/products/types/${product?.type}`} className="tag">
+                                    {product?.type}
+                                </Link>
+                                <Link href={`/products/${product?.gender}`} className="tag">
+                                    {product?.gender}
+                                </Link>
+                            </div>
+                            <div className="py-5">
+                                <button
+                                    className="w-full text-white dark:text-black dark:bg-white bg-black py-2 px-3 hover:text-white hover:bg-primary duration-300 rounded-full disabled:bg-gray-400 disabled:cursor-not-allowed"
+                                    onClick={addToCartHandler}
+                                    disabled={submitting}
+                                >
+                                    + add to cart
+                                </button>
+                                {success === true && (
+                                    <BsCartCheckFill className="text-green-400 absolute top-3 left-2 w-7 h-7" />
+                                )}
+                                {success === false && (
+                                    <IoMdCloseCircle className="text-red-600 absolute top-3 left-2 w-7 h-7" />
+                                )}
+                            </div>
                         </div>
-                        <div className="text-right text-black dark:text-white pt-4 pb-5 border-b border-gray-200 dark:border-dark-container">
-                            <p className="text-sm">price cash</p>
-                            <p className="text-right text-primary mt-5">
-                                {product?.price} EGP
-                            </p>
+
+                        <div className="flex-1">
+                            <div className="grid grid-cols-2 gap-4">
+                                {product?.images.map((image) => (
+                                    <Image
+                                        key={image}
+                                        src={image}
+                                        width={408}
+                                        height={612}
+                                        quality={100}
+                                        alt={product.title}
+                                        className="w-full max-lg:odd:last-of-type:col-span-2"
+                                    />
+                                ))}
+                            </div>
                         </div>
-                        <div className="flex justify-end gap-2 py-5 border-b border-gray-200 dark:border-dark-container">
-                            <Link
-                                href={`/products/categories/${product?.category}`}
-                                className="tag"
-                            >
-                                {product?.category}
-                            </Link>
-                            <Link href={`/products/types/${product?.type}`} className="tag">
-                                {product?.type}
-                            </Link>
-                            <Link href={`/products/${product?.gender}`} className="tag">
-                                {product?.gender}
-                            </Link>
-                        </div>
-                        <div className="py-5">
-                            <button
-                                className="w-full text-white dark:text-black dark:bg-white bg-black py-2 px-3 hover:text-white hover:bg-primary duration-300 rounded-full disabled:bg-gray-400 disabled:cursor-not-allowed"
-                                onClick={addToCartHandler}
-                                disabled={submitting}
-                            >
-                                + add to cart
-                            </button>
-                            {success === true && (
-                                <BsCartCheckFill className="text-green-400 absolute top-3 left-2 w-7 h-7" />
-                            )}
-                            {success === false && (
-                                <IoMdCloseCircle className="text-red-600 absolute top-3 left-2 w-7 h-7" />
-                            )}
-                        </div>
-                    </div>
-                
-                <div className="flex-1">
-                    <div className="grid grid-cols-2 gap-4">
-                        {product?.images.map((image) => (
-                            <Image
-                                key={image}
-                                src={image}
-                                width={408}
-                                height={612}
-                                quality={100}
-                                alt={product.title}
-                                className="w-full max-lg:odd:last-of-type:col-span-2"
-                            />
-                        ))}
-                    </div>
-                </div>
-                </>
+                    </>
                 )}
             </div>
         </section>
