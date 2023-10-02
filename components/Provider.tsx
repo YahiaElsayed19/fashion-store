015e0091febadc92c5dd3913a2bbb2280a71ce9e
Yahia Elsayed <yahiaelsayed19@gmail.com>
2023-10-02 21:47:46 +0300

    fix session type

--- components/Provider.tsx
+++ components/Provider.tsx
@@ -1,7 +1,8 @@
+import { Session } from 'next-auth'
 import { SessionProvider } from 'next-auth/react'
 import React from 'react'
 
-const Provider = ({ children, session }: { children: React.ReactNode, session?: any }) => {
+const Provider = ({ children, session }: { children: React.ReactNode, session?: Session }) => {
     return (
         <SessionProvider session={session}>{children}</SessionProvider>
     )
