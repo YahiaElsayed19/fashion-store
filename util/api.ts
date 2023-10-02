103cca1ea5cd89c7aac93ed91d81979932d7cd96
Yahia Elsayed <yahiaelsayed19@gmail.com>
2023-10-02 21:00:40 +0300

    update cart req and add to wishlist req

--- util/api.ts
+++ util/api.ts
@@ -3,31 +3,49 @@ const API = axios.create({
     baseURL: `/api/`,
 });
 
-
 export const getProducts = (page?: number) => {
-    return API.get(`products`, { params: { page: page } })
-}
+    return API.get(`products`, { params: { page: page } });
+};
 
-export const getProduct = (productId?: string) => {
-    return API.get(`products/product?product-id=${productId}`)
-}
+export const getProduct = (productId?: string, authorization?: string) => {
+    return API.get(`products/product?product-id=${productId}`, {
+        headers: { Authorization: authorization },
+    });
+};
 
 export const getProductsByType = (type: string, page?: number) => {
-    return API.get(`products/types/${type}`, { params: { page: page } })
-}
+    return API.get(`products/types/${type}`, { params: { page: page } });
+};
 
 export const getProductsByCategory = (category: string, page?: number) => {
-    return API.get(`products/categories/${category}`, { params: { page: page } })
-}
+    return API.get(`products/categories/${category}`, { params: { page: page } });
+};
 
 export const getProductsByGender = (gender: string, page?: number) => {
-    return API.get(`products/${gender}`, { params: { page: page } })
-}
+    return API.get(`products/${gender}`, { params: { page: page } });
+};
 
-export const addToCart = (userId: string, productId: string) => {
-    return API.post(`cart/${userId}`, {}, { params: { 'product-id': productId } })
-}
+export const addToCart = (authorization: string, productId: string) => {
+    return API.post(
+        `cart`,
+        {},
+        {
+            headers: { Authorization: authorization },
+            params: { "product-id": productId },
+        }
+    );
+};
+export const addToWishlist = (authorization: string, productId: string) => {
+    return API.post(
+        `wishlist`,
+        {},
+        {
+            headers: { Authorization: authorization },
+            params: { "product-id": productId },
+        }
+    );
+};
 
 export const searchProducts = (search: string) => {
-    return API.get(`search`, { params: { search: search } })
-}
+    return API.get(`search`, { params: { search: search } });
+};
