10c4847777b8341dd377c3886d17bf756b5c261b
Yahia Elsayed <yahiaelsayed19@gmail.com>
2023-10-04 00:41:28 +0300

    optimize styles and solve dehydration problem

--- components/ThemeSwitcher.tsx
+++ components/ThemeSwitcher.tsx
@@ -4,16 +4,16 @@ import { useTheme } from "next-themes";
 import { BsMoonStarsFill, BsSunFill } from "react-icons/bs";
 import { RiComputerFill } from "react-icons/ri";
 const ThemeSwitcher = () => {
-    // const [mounted, setMounted] = useState(false)
+    const [mounted, setMounted] = useState(false)
     const { theme, setTheme } = useTheme();
     const [showOptions, setShowOptions] = useState(false);
-    // useEffect(() => {
-    //     setMounted(true)
-    // }, [])
+    useEffect(() => {
+        setMounted(true)
+    }, [])
 
-    // if (!mounted) {
-    //     return null
-    // }
+    if (!mounted) {
+        return null
+    }
 
     return (
         <div className="relative">
@@ -23,15 +23,15 @@ const ThemeSwitcher = () => {
                     setShowOptions((prev) => !prev);
                 }}
             >
-                {theme == "dark" && <BsMoonStarsFill className=" w-8 h-8 " />}
-                {theme == "light" && <BsSunFill className=" w-8 h-8 " />}
-                {theme == "system" && <RiComputerFill className=" w-8 h-8 " />}
+                {theme == "dark" && <BsMoonStarsFill className=" theme-icon" />}
+                {theme == "light" && <BsSunFill className="theme-icon" />}
+                {theme == "system" && <RiComputerFill className=" theme-icon" />}
                 {showOptions && (
-                    <div className="absolute bottom-[74px] right-0 p-3 rounded-lg bg-gray-200 dark:bg-dark-container">
+                    <div className="absolute bottom-[64px] right-0 p-3 rounded-lg bg-gray-200 dark:bg-dark-container">
                         <button
                             aria-label="dark button"
                             type="button"
-                            className="flex gap-2 p-1 justify-center items-center text-black dark:text-white"
+                            className={`theme-button ${theme === 'dark' ? "text-primary":"text-black dark:text-white"}`}
                             onClick={() => {
                                 setTheme("dark");
                             }}
@@ -41,7 +41,7 @@ const ThemeSwitcher = () => {
                         <button
                             aria-label="light button"
                             type="button"
-                            className="flex gap-2 p-1 justify-center items-center text-black dark:text-white"
+                            className={`theme-button ${theme === 'light' ? "text-primary":"text-black dark:text-white"}`}
                             onClick={() => {
                                 setTheme("light");
                             }}
@@ -51,7 +51,7 @@ const ThemeSwitcher = () => {
                         <button
                             aria-label="system button"
                             type="button"
-                            className="flex gap-2 p-1 justify-center items-center text-black dark:text-white"
+                            className={`theme-button ${theme === 'system' ? "text-primary":"text-black dark:text-white"}`}
                             onClick={() => {
                                 setTheme("system");
                             }}
