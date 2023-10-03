070764eb1c10508b0ad34fb287e793677675b9cb
Yahia Elsayed <yahiaelsayed19@gmail.com>
2023-10-03 20:07:54 +0300

    authentication redirects

--- app/wishlist/page.tsx
+++ app/wishlist/page.tsx
@@ -1,6 +1,9 @@
 import React from 'react'
-
+import { NextResponse } from 'next/server'
 const page = () => {
+  const res= NextResponse.next().cookies
+  console.log(res);
+  
   return (
     <div>page</div>
   )
