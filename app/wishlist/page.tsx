6f5f4159dfedcd83abc4fa0aff2773d9fd7c6fad
Yahia Elsayed <yahiaelsayed19@gmail.com>
2023-10-04 05:22:22 +0300

    finish wishlist page

--- app/wishlist/page.tsx
+++ app/wishlist/page.tsx
@@ -1,8 +1,46 @@
-import React from 'react'
+"use client";
+import React, { useEffect, useState } from "react";
+import { getSession, useSession } from "next-auth/react";
+import { getWishlist, removeWishlist } from "@util/api";
+import ProductsList from "@components/product/ProductsList";
+import { productType } from "@types";
+import { useRouter } from "next/navigation";
 const page = () => {
+  const { data: session } = useSession();
+  const router = useRouter();
+  const [products, setProducts] = useState<productType[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+  useEffect(() => {
+    (async () => {
+      setLoading(true);
+      const session = await getSession();
+      try {
+        //@ts-ignore
+        const { data } = await getWishlist(session?.user.id);
+        setProducts(data);
+      } catch (error) { }
+      setLoading(false);
+    })();
+  }, []);
+  const emptyWishlistHandler = async () => {
+    //@ts-ignore
+    await removeWishlist(session?.user.id);
+    setProducts([]);
+  };
   return (
-    <div>page</div>
-  )
-}
+    <section className="page">
+      <ProductsList products={products} title="Wishlist" loading={loading} />
+      <button
+        aria-label="delete wishlist"
+        type="button"
+        className="py-2 px-3 text-white bg-red-600 font-bold rounded-full disabled:bg-gray-500 disabled:cursor-not-allowed"
+        onClick={emptyWishlistHandler}
+        disabled={products.length === 0}
+      >
+        Empty Wishlist
+      </button>
+    </section>
+  );
+};
 
-export default page
+export default page;
--- components/product/ProductsList.tsx
+++ components/product/ProductsList.tsx
@@ -10,7 +10,8 @@ const ProductsList = ({ products, title, loading }: { title: string, products: p
             <h1 className='text-2xl font-bold uppercase text-center text-black dark:text-white'><span className='text-primary'>{title}</span> products</h1>
             <div className='container w-full max-w-6xl p-5 mx-auto '>
                 <div className='flex justify-center flex-wrap gap-10 p-5 rounded-xl bg-gray-200 dark:bg-dark-container'>
-                    {products?.map((product: productType) => <ProductCard key={product._id} id={product._id} title={product.title} imageSrc={product.images[0]} price={product.price} />)}
+                    {products.length > 0 && products?.map((product: productType) => <ProductCard key={product._id} id={product._id} title={product.title} imageSrc={product.images[0]} price={product.price} />)}
+                    {!loading && products.length == 0 && <p className='font-bold text-black dark:text-white'>There is no products</p>}
                     {loading && <ProductCardPlaceholder count={20} />}
                 </div>
             </div>
