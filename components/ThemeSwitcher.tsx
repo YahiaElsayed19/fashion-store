b570bd17b0891be07203d9e0720b4154f020efd1
Yahia Elsayed <yahiaelsayed19@gmail.com>
2023-10-05 04:45:47 +0300

    end hydration problem

--- components/ThemeSwitcher.tsx
+++ components/ThemeSwitcher.tsx
@@ -1,20 +1,12 @@
 "use client";
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { useTheme } from "next-themes";
 import { BsMoonStarsFill } from "react-icons/bs";
 import { FaSun } from "react-icons/fa";
 import { RiComputerFill } from "react-icons/ri";
 const ThemeSwitcher = () => {
-    const [mounted, setMounted] = useState(false)
     const { theme, setTheme } = useTheme();
     const [showOptions, setShowOptions] = useState(false);
-    useEffect(() => {
-        setMounted(true)
-    }, [])
-
-    if (!mounted) {
-        return null
-    }
 
     return (
         <div className="relative">
@@ -24,15 +16,14 @@ const ThemeSwitcher = () => {
                     setShowOptions((prev) => !prev);
                 }}
             >
-                {theme == "dark" && <BsMoonStarsFill className=" theme-icon" />}
-                {theme == "light" && <FaSun className="theme-icon" />}
-                {theme == "system" && <RiComputerFill className=" theme-icon" />}
+                <BsMoonStarsFill className="theme-icon" />
                 {showOptions && (
                     <div className="absolute bottom-[64px] right-0 rounded-lg bg-gray-200 dark:bg-dark-container">
                         <button
                             aria-label="dark button"
                             type="button"
-                            className={`theme-button ${theme === 'dark' ? "text-primary":"text-black dark:text-white"}`}
+                            className={`theme-button ${theme === "dark" ? "text-primary" : "text-black dark:text-white"
+                                }`}
                             onClick={() => {
                                 setTheme("dark");
                             }}
@@ -42,7 +33,10 @@ const ThemeSwitcher = () => {
                         <button
                             aria-label="light button"
                             type="button"
-                            className={`theme-button ${theme === 'light' ? "text-primary":"text-black dark:text-white"}`}
+                            className={`theme-button ${theme === "light"
+                                    ? "text-primary"
+                                    : "text-black dark:text-white"
+                                }`}
                             onClick={() => {
                                 setTheme("light");
                             }}
@@ -52,7 +46,10 @@ const ThemeSwitcher = () => {
                         <button
                             aria-label="system button"
                             type="button"
-                            className={`theme-button ${theme === 'system' ? "text-primary":"text-black dark:text-white"}`}
+                            className={`theme-button ${theme === "system"
+                                    ? "text-primary"
+                                    : "text-black dark:text-white"
+                                }`}
                             onClick={() => {
                                 setTheme("system");
                             }}
