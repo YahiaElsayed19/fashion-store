ebc47b35fce45253bbe99c9cc780d5437fc366be
Yahia Elsayed <yahiaelsayed19@gmail.com>
2023-10-06 01:00:20 +0300

    solve autoplay

--- components/carousel/ProductsCarousel.tsx
+++ components/carousel/ProductsCarousel.tsx
@@ -9,7 +9,7 @@ import { Splide } from '@splidejs/react-splide'
 import '@splidejs/react-splide/css';
 import SlidePlaceholder from '@components/placeholder/SlidePlaceholder';
 
-const ProductsCarousel = ({ queryFunction, title, url }: { queryFunction:  (title: string) => Promise<AxiosResponse<any, any>>, title: string, url: string }) => {
+const ProductsCarousel = ({ queryFunction, title, url }: { queryFunction: (title: string) => Promise<AxiosResponse<any, any>>, title: string, url: string }) => {
     const { data, isLoading } = useQuery(title, () => queryFunction(title))
     return (
         <section className='py-[50px] bg-white dark:bg-black'>
@@ -22,13 +22,17 @@ const ProductsCarousel = ({ queryFunction, title, url }: { queryFunction:  (titl
                     {isLoading ?
                         <Splide options={{
                             gap: '1rem',
+                            rewind: true,
                             arrows: false,
                             pagination: false,
                             autoWidth: true,
+                            perMove: 2,
+                            autoplay: true,
+                            pauseOnHover: true,
                         }}>
-                            <SlidePlaceholder count={20}/>
+                            <SlidePlaceholder count={20} />
                         </Splide>
-                        : <Splide options={{
+                        : (<Splide options={{
                             gap: '1rem',
                             rewind: true,
                             arrows: false,
@@ -40,7 +44,7 @@ const ProductsCarousel = ({ queryFunction, title, url }: { queryFunction:  (titl
                         }}
                         >
                             {data?.data.map((product: productType) => <ProductSlide key={product._id} id={product._id} title={product.title} price={product.price} imageSrc={product.images[0]} />)}
-                        </Splide>}
+                        </Splide>)}
                 </div>
             </div>
         </section>
