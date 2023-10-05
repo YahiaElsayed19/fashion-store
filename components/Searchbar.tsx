29e0368dc8ef288a44f9cd30a89da26cb655ea51
Yahia Elsayed <yahiaelsayed19@gmail.com>
2023-10-05 23:38:27 +0300

    optimize accessability search button

--- components/Searchbar.tsx
+++ components/Searchbar.tsx
@@ -2,19 +2,19 @@ import React, { FormEvent, useRef } from "react";
 import { useRouter } from "next/navigation";
 import { RiSearchLine } from "react-icons/ri";
 const Searchbar = ({ closeMenuHandler }: { closeMenuHandler?: () => void }) => {
-    const searchRef = useRef<HTMLInputElement>(null)
-    const router = useRouter()
+    const searchRef = useRef<HTMLInputElement>(null);
+    const router = useRouter();
     const searchHandler = (e: FormEvent) => {
-        e.preventDefault()
+        e.preventDefault();
         if (searchRef.current) {
             if (searchRef.current.value === "") {
-                router.push(`/`)
+                router.push(`/`);
             } else {
-                router.push(`/search?search=${searchRef.current.value}`)
+                router.push(`/search?search=${searchRef.current.value}`);
             }
         }
-        if (closeMenuHandler) closeMenuHandler()
-    }
+        if (closeMenuHandler) closeMenuHandler();
+    };
     return (
         <form className="flex flex-col items-center justify-center relative w-full">
             <input
@@ -23,8 +23,13 @@ const Searchbar = ({ closeMenuHandler }: { closeMenuHandler?: () => void }) => {
                 className="input"
                 ref={searchRef}
             />
-            <button type="submit" title="search" className="absolute top-[50%] translate-y-[-50%] right-3 p-2" onClick={searchHandler}>
-                {" "}
+            <button
+                aria-label="search button"
+                type="submit"
+                title="search"
+                className="absolute top-[50%] translate-y-[-50%] right-3 p-2"
+                onClick={searchHandler}
+            >
                 <RiSearchLine className=" text-black w-5 h-5 sm:w-6 sm:h-6 cursor-pointer dark:text-white" />
             </button>
         </form>
